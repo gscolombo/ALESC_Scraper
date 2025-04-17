d1508931aea4d40f2dbbd8138908f3227e696e48
Gabriel S. Colombo <gscolombo404@gmail.com>
2025-04-16 21:07:33 -0300

    Data serialization and storage in JSON format

--- src/main.py
+++ src/main.py
@@ -1,9 +1,16 @@
+import os
 from datetime import datetime
+from pathlib import Path
+from json import dump
+
 from crawler import crawler
+
+# It's expected that this script will be called from the root path
+ROOT_PATH = os.getcwd()
 
 
 def get_data_input() -> str:
-    date = input()
+    date = input("Insert a date in ISO format: ")
     try:
         return datetime.fromisoformat(date).strftime("%d/%m/%Y")
     except ValueError:
@@ -15,6 +22,24 @@ if __name__ == "__main__":
     if start_date is not None:
         print(f"Start date: {start_date}")
 
-        panels = crawler(start_date)
+        print("Data retrieved.")
+        pages = crawler(start_date)
 
-        print(panels)
+        print("Flattening data...")
+        data = [
+            event.to_dict()
+            for page in pages
+            for panel in page
+            for event in panel
+        ]
+
+        data_dir = Path(ROOT_PATH, 'data')
+        data_dir.mkdir(exist_ok=True)
+
+        save_path = data_dir.joinpath(
+            f"events_starting_from_{start_date.replace('/', '-')}.json")
+
+        print(f"Storing data at {save_path}...")
+        with open(save_path, "w") as fp:
+            dump(data, fp, indent=4, ensure_ascii=False)
+            print(f"Data stored.")
