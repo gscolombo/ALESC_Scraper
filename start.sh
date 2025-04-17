ea6ee880f3596fcfd3f01c249a1728f427c1a28b
Gabriel S. Colombo <gscolombo404@gmail.com>
2025-04-16 21:50:18 -0300

    Script to start crawler

---
+++ start.sh
@@ -0,0 +1,12 @@
+#! usr/bin/sh
+
+image="gscolombo/alesc-crawler:latest"
+
+
+if [ -z "$(docker images -q $image 2> /dev/null)" ]; then
+    echo "Image \"$image\" not found.\nStarting build...\n"
+    docker build -t $image .
+fi
+echo "\n"
+clear
+docker run -itv ./data:/home/datapolicy/data $image python src/main.py
