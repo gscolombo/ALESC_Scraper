6af903a48d3bcb437c7d288ce3dc4ae7920c40e3
Gabriel S. Colombo <gscolombo404@gmail.com>
2025-04-16 21:03:18 -0300

    Implementation of method for Event model dumping

--- src/Event.py
+++ src/Event.py
@@ -12,3 +12,14 @@ class Event(BaseModel):
     local: str
     organizer: str | None
     contact: Contact
+
+    def to_dict(self):
+        if (self.start_date is not None
+                and isinstance(self.start_date, datetime)):
+            self.start_date = self.start_date.isoformat(timespec="seconds")
+
+        if (self.end_date is not None
+                and isinstance(self.end_date, datetime)):
+            self.end_date = self.end_date.isoformat(timespec="seconds")
+
+        return self.model_dump(warnings=False)
